24afd5a006301d5949f0be93245829788cf89dcf
Jonatas00 <djonatas31@gmail.com>
2025-04-10 22:39:08 -0300

    Initial commit

---
+++ app/src/main/java/com/jonatas00/finanx/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.jonatas00.finanx.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/jonatas00/finanx/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.jonatas00.finanx.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
